bc873c7ebd17de208aedc6b6d409509f34099861
Peter Brottveit Bock <post@peterbb.net>
2017-02-07 02:43:02 +0100

    removed comments

--- core/ast.ml
+++ core/ast.ml
@@ -77,63 +77,14 @@ end = struct
             | (Con _ | MVar _) as h -> h
         in abs
 
-    (* [subst e lvl f] is:
-     *
-     *    G D : e      G * D : f
-     * -------------------------------
-     *          G D : subst e lvl f
-     * where lvl = |G|.
-     *)
     let rec subst e lvl = function
-        (*            * G * D : f
-         *           --------------
-         *  G D : e   G * D : (Lam f)
-         * ------------------------------
-         *   G D : subst e lvl (Lam f)
-         *)
         | Lam (x, body) ->
-            (*     G D : e 
-             *  -------------------
-             *   * G D : (shift e)       * G * D : f
-             *  --------------------------------------
-             *   * G D : subst (shift e) (lvl + 1) f
-             *  -----------------------------------------
-             *   G D : Lam (subst (shift e) (lvl + 1) f)
-             *)
             Lam (x, subst (shift e) (lvl + 1) body)
-
-        (*
-         *              ---------------
-         *  G D : e       G * D : (Var lvl)
-         * ---------------------------------
-         *    G D : subst e lvl (Var lvl)
-         *)
         | App (Var i, spine) when i = lvl ->
-            (* 
-             *  G D : e
-             *)
             redex e (List.map (subst e lvl) spine)
-
-        (*
-         *   G D * D' : e    G * D * D' : Var (|D|+1+|D|)
-         * ------------------------------------------------
-         *      G D * D' : subst e lvl (Var |G|+1+|D|)
-         *)
         | App (Var i, spine) when i > lvl ->
-            (*
-             *    G D * D' : Var (|G|+|D|)
-             *)
             App (Var (i - 1), List.map (subst e lvl) spine)
-
-        (*
-        *   G * G' D : e        G * G' * D : Var |G|
-        * ----------------------------------------------
-        *     G * D' D : subst e lvl (Var |G|)
-        *)
         | App (Var i, spine) ->
-            (*
-             * G * G' D : Var |G|
-             *)
             App (Var i, List.map (subst e lvl) spine)
         | App (Con c, spine) ->
             App (Con c, List.map (subst e lvl) spine)
@@ -141,14 +92,10 @@ end = struct
             App (MVar m, List.map (subst e lvl) spine)
 
     and redex e spine = match e, spine with
-        (* (x @ s) @ s'  ->   x @ (s @ s') *)
         | App (head, spine'), _ ->
             App (head, spine' @ spine)
-
-        (* (\f) @ (e : s)  ->  (subst e 0 f) @ s *)
         | Lam (x, body), e' :: spine' ->
             redex (subst e' 0 body) spine'
-
         | Lam (x, body), [] ->
             Lam (x, body)
  
@@ -156,28 +103,9 @@ end = struct
         let rec shift_term lvl = function
             | App (head, spine) ->
                 App (shift_head lvl head, shift_spine lvl spine)
-            (*
-             *       * G D : e
-             *    -------------
-             *      G D : Lam e
-             * ---------------------------
-             *  G * D : shift lvl (Lam e)
-             *)
             | Lam (x, e) ->
-                (*
-                 *     * G D : e
-                 * -----------------------------
-                 *   * G * D : shift (lvl+1) e
-                 * ----------------------------
-                 *   G * D : Lam (shift (lvl+1) e)
-                 *)
                 Lam (x, shift_term (1 + lvl) e)
         and shift_head lvl = function
-            (*
-             *       G * G' D : Var (i=G)
-             *  ------------------------------------
-             *    G * G' * D  : shift (G+1+G') (Var G)
-             *)
             | Var i when i < lvl -> Var i
             | Var i -> Var (i + 1)
             | Con c -> Con c
